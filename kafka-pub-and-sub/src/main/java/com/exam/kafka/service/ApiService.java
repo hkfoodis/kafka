cdbd073348131c33f8e3fe19af57813f09a41aec
hkfoodis <dlaehdud96@naver.com>
2022-06-20 16:32:13 +0900

    feat: Calculator 서비스에 연결할 서비스 생성 ApiService.java 생성

---
+++ kafka-pub-and-sub/src/main/java/com/exam/kafka/service/ApiService.java
@@ -0,0 +1,59 @@
+package com.exam.kafka.service;
+
+import com.exam.kafka.domain.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Service
+@Slf4j
+public class ApiService {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+
+    private HttpEntity<?> getEntity(Object params) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-type", org.springframework.http.MediaType.APPLICATION_JSON_VALUE);
+        return params != null ? new HttpEntity<>(params, httpHeaders) : new HttpEntity<>(httpHeaders);
+    }
+
+    private <T> T execute(URI uri, HttpMethod method, Object params, Class<T> clazz) {
+        try {
+            return restTemplate.exchange(uri, method, getEntity(params), clazz).getBody();
+        } catch (Exception exception) {
+            // TODO S3 에러 사례 작성 프로세스 개발
+            try {
+                String json = objectMapper.writeValueAsString(params);
+                log.error("errors on api: {}, request params: {}", uri, json, exception);
+            }
+            catch(JsonProcessingException je) {
+                log.error("errors on api: {}, parsing error params: {}", params, je, exception);
+            }
+
+            return null;
+        }
+    }
+
+    public User calAvg(User user) {
+        String url = "";
+
+        User response = execute(URI.create(url), HttpMethod.POST, user, User.class);
+
+        if (response == null) {
+            return null;
+        }
+
+        return response;
+    }
+}
